22a8281b8b86728dbfa652bc9afc24289e74d2e4
Ryan Bliss <ryanbliss@microsoft.com>
2023-10-17 21:54:41 -0700

    Implemented color picker for offline 3D model

--- src/components/OfflineModelViewer.tsx
+++ src/components/OfflineModelViewer.tsx
@@ -1,7 +1,15 @@
-import { FC, useRef, useState } from "react";
+import { FC, useRef, useState, useCallback, useEffect } from "react";
 import { ModelViewerScene } from "./ModelViewerScene";
-import { Nullable, Scene as BabyScene, ArcRotateCamera } from "@babylonjs/core";
+import {
+    Nullable,
+    Scene as BabyScene,
+    ArcRotateCamera,
+    PickingInfo,
+} from "@babylonjs/core";
+import { IPointerEvent } from "@babylonjs/core/Events";
 import { HexColorPicker } from "react-colorful";
+import { Color3 } from "@babylonjs/core/Maths/math";
+import { PBRMaterial } from "@babylonjs/core/Materials";
 
 export const OfflineModelViewer: FC = () => {
     // Babylon scene reference
@@ -18,6 +26,68 @@ export const OfflineModelViewer: FC = () => {
      * Colors map
      */
     const [colorsMap] = useState<Map<string, string>>(new Map());
+
+    /**
+     * Callback for when the local user selected a new color to apply to the 3D model
+     */
+    const onChangeColor = useCallback(
+        (value: string) => {
+            if (!selectedMaterialName) return;
+            if (!sceneRef.current) return;
+            try {
+                colorsMap.set(selectedMaterialName, value);
+                const material =
+                    sceneRef.current.getMaterialByName(selectedMaterialName);
+                if (material && material instanceof PBRMaterial) {
+                    const color = Color3.FromHexString(value);
+                    material.albedoColor = color;
+                }
+            } catch (err: any) {
+                console.error(err);
+            }
+        },
+        [colorsMap, selectedMaterialName]
+    );
+
+    /**
+     * Callback for when the user clicks on the scene
+     */
+    const handlePointerDown = useCallback(
+        (evt: IPointerEvent) => {
+            if (!sceneRef.current) return;
+
+            const pickResult: Nullable<PickingInfo> = sceneRef.current.pick(
+                evt.clientX,
+                evt.clientY
+            );
+
+            if (pickResult && pickResult.hit && pickResult.pickedMesh) {
+                const mesh = pickResult.pickedMesh;
+                if (!mesh.material) return;
+                // When the user clicks on a specific material in our object, we set it as selected to show the color picker
+                setSelectedMaterialName(mesh.material.name);
+                return;
+            }
+            if (!selectedMaterialName) return;
+            setSelectedMaterialName(null);
+        },
+        [selectedMaterialName]
+    );
+
+    /**
+     * Setup the onPointerDown event listener
+     */
+    useEffect(() => {
+        if (sceneRef.current) {
+            sceneRef.current.onPointerDown = handlePointerDown;
+        }
+        return () => {
+            if (sceneRef.current) {
+                sceneRef.current.onPointerDown = undefined;
+            }
+        };
+    }, [handlePointerDown]);
+
     return (
         <>
             {!!selectedMaterialName && (
@@ -27,13 +97,12 @@ export const OfflineModelViewer: FC = () => {
                             ? colorsMap.get(selectedMaterialName)
                             : undefined
                     }
-                    onChange={(color) => {
-                        colorsMap.set(selectedMaterialName, color);
-                    }}
+                    onChange={onChangeColor}
                     style={{
                         position: "absolute",
                         top: "72px",
                         right: "24px",
+                        zIndex: 1,
                     }}
                 />
             )}
